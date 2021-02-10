092ed7a55352aef77789f00a68efaae6a1fd843c
Coreen Cooper <coreencooper@pursuit.org>
2021-02-10 10:25:26 -0500

    solve drawSquare()

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -52,9 +52,17 @@ Output:
     ***
 ]
   */
-const drawSquare = () => {
-  
+const drawSquare = (num) => {
+  let matrix = [];
+  let sum = ""
+  for (i = 1; i <= num; i++){
+    sum += "*"
+  } for (i = 1; i <= num; i++) {
+    matrix.push(sum)
+  } 
+  return matrix
 }
+
 /**
   * If we list all the natural numbers below 10 that are multiples of 3 or 5,
   *  we get 3, 5, 6 and 9. The sum of these multiples is 23.
